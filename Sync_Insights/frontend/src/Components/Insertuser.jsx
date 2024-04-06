8aab2771ee2958e6ae7aaaebd6598ec8119aeec7
Dhruvin Malani <malanidhruvin21@gmail.com>
2024-04-06 14:51:22 +0530

    updated!

--- Sync_Insights/frontend/src/Components/Insertuser.jsx
+++ Sync_Insights/frontend/src/Components/Insertuser.jsx
@@ -1,8 +1,8 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { InsertUser } from "../Global/apiCall";
 import './Insertuser.css'
 function Insert_user() {
-   const [insertUser, setinsertUser] = useState({ fullname: "",username:"",email:"",department:"",position:"", password: "" });
+  const [insertUser, setinsertUser] = useState({ fullname: "", username: "", email: "", department: "", position: "", password: "" });
 
   const getInfo = (i) => {
     console.log(i.target.name, i.target.value);
@@ -23,23 +23,25 @@ function Insert_user() {
   };
   return (
     <div className='act-user'>
-        <div className='user-add'>
-            <input type="text" name="fullname" placeholder="fullname" onChange={getInfo} />
-            <input type="text" name="username" placeholder="username" onChange={getInfo} />
-            <input type="email" name="email" placeholder="email" onChange={getInfo} />
-            <select name="department" onChange={getInfo}  id="department">
-              <option value="Sales">Sales</option>
-              <option value="Finance">Finance</option>
-              <option value="Operations">Operations</option>
-            </select>
-            <select name="position" id="position" onChange={getInfo} >
-              <option value="Manager">Manager</option>
-              <option value="Associate">Associate</option>
-              <option value="Analyst">Analyst</option>
-            </select>            
-            <input type="password" name="password" placeholder="password" onChange={getInfo} />
-            <button onClick={handleInsertUser}>Add User</button>
-        </div>
+      <div className='user-add'>
+        <input type="text" name="fullname" placeholder="fullname" onChange={getInfo} />
+        <input type="text" name="username" placeholder="username" onChange={getInfo} />
+        <input type="email" name="email" placeholder="email" onChange={getInfo} />
+        <select name="department" onChange={getInfo} id="department">
+        <option value="" aria-readonly>Select Department</option>
+          <option value="Sales">Sales</option>
+          <option value="Finance">Finance</option>
+          <option value="Operations">Operations</option>
+        </select>
+        <select name="position" id="position" onChange={getInfo} >
+        <option value="" aria-readonly>Select Position</option>
+          <option value="Manager">Manager</option>
+          {/* <option value="Associate">Associate</option>
+          <option value="Analyst">Analyst</option> */}
+        </select>
+        <input type="password" name="password" placeholder="password" onChange={getInfo} />
+        <button onClick={handleInsertUser}>Add User</button>
+      </div>
     </div>
   )
 }
